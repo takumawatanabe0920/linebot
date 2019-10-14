eb16388c13a5786eef1d321c760c43f4e9e42e04
渡辺拓馬 <>
2019-10-14 22:57:33 +0900

    aaa

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -31,7 +31,7 @@ private
   def client
     @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      config.channel_token = ENV["LINE_ACCESS_TOKEN"]
     }
   end
 end
